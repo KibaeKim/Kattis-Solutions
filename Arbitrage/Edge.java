5dd27b83e6b163fe2029d8ce7bafd927cafee9a6
KibaeKim <kibaekim@uvic.ca>
2019-03-15 19:52:38 -0700

    Create Edge.java

---
+++ Arbitrage/Edge.java
@@ -0,0 +1,10 @@
+public class Edge{
+    String u,v;
+    double weight;
+
+    public Edge (String u, String v, double weight){
+        this.u = u;
+        this.v = v;
+        this.weight = weight;
+    }
+}
