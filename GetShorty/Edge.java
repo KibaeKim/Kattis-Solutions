3a213c3bb27ad98a5e2d123215e6a7e6cf080b16
KibaeKim <kibaekim@uvic.ca>
2019-03-15 19:47:32 -0700

    Create Edge.java

---
+++ GetShorty/Edge.java
@@ -0,0 +1,10 @@
+public class Edge{
+    int u,v;
+    double weight;
+
+    public Edge (int u, int v, double weight){
+        this.u = u;
+        this.v = v;
+        this.weight = weight;
+    }
+}
